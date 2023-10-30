b04c2f702fea68f9161c70d5fe4f0f3a31ef6fa8
Pedro Bose Ximenes <92697766+pedroboseximenes@users.noreply.github.com>
2023-10-30 17:34:14 -0300

    Adicionado buscaNome e deletarCliente

--- Utils.py
+++ Utils.py
@@ -2,49 +2,54 @@ from Cliente import Cliente
 
 
 def recebeUsuario():
-    print("\n------------- Login --------------")
-    nome = input('> Informe seu usuario: ')
-    ip = input('> Informe seu IP: ')
-    usuario = Cliente(nome, ip)
-    return usuario
+  print("\n------------- Login --------------")
+  nome = input('> Informe seu usuario: ')
+  ip = input('> Informe seu IP: ')
+  usuario = Cliente(nome, ip)
+  return usuario
+
+
+def buscarClientePeloNome():
+  pass
 
 def imprimeListaUsuarios(listaUsuarios):
-    print("\n--------- Lista de Usuários Cadastrados ---------")
-    for usuario in listaUsuarios:
-        print("\n")
-        print(f'> NOME: {usuario.nome}')
-        print(f'> IP: {usuario.ip}')
-        print("---------------")
+  print("\n--------- Lista de Usuários Cadastrados ---------")
+  for usuario in listaUsuarios:
+    print("\n")
+    print(f'> NOME: {usuario.nome}')
+    print(f'> IP: {usuario.ip}')
+    print("---------------")
+
 
 def menuUsuario(conexao, usuario):
-    print("\n-------------------- Menu --------------------")
-    print("Escolha uma opção: \n")
-    print("1 - Lista todas os usuários cadastrados")
-    print("2 - Buscar pelo nome de um usuário")
-    print("3 - Buscar pelo ip de um usuário")
-    print("4 - Desligar conexão com servidor")
-    print("5 - Sair")
-    resposta = int(input("\n> "))
+  print("\n-------------------- Menu --------------------")
+  print("Escolha uma opção: \n")
+  print("1 - Lista todas os usuários cadastrados")
+  print("2 - Buscar pelo nome de um usuário")
+  print("3 - Buscar pelo ip de um usuário")
+  print("4 - Desligar conexão com servidor e sair")
+  resposta = int(input("\n> "))
 
-    if (resposta == 1):
-        usuario.enviaMensagem(conexao,"listagem")
-        listaUsuarios = usuario.recebeMensagem(conexao)
-        imprimeListaUsuarios(listaUsuarios)
-    elif (resposta == 2):
-        usuario.enviaMensagem(conexao,"buscarNome")
+  if (resposta == 1):
+    usuario.enviaMensagem(conexao, "listagem")
+    listaUsuarios = usuario.recebeMensagem(conexao)
+    imprimeListaUsuarios(listaUsuarios)
+  elif (resposta == 2):
+    usuario.enviaMensagem(conexao, "buscarNome")
 
-        nomeProcurado = input("Digite o nome do Cliente que deseja procurar:\n")
-        print("Procurando", nomeProcurado)
-        usuario.enviaMensagem(conexao, nomeProcurado)
-        clienteProcurado = usuario.recebeMensagem(conexao)
-        if(clienteProcurado != []):
-            print(f"Cliente encontrado! \n Nome: {clienteProcurado.nome}\n IP: {clienteProcurado.ip}\n")
-        else:
-            print("Cliente com esse nome não encontrado! Tente novamente\n")
-    elif (resposta == 3):
-        pass
-    elif(resposta ==4):
-        usuario.enviaMensagem(conexao,"desligar")
-        
-    elif (resposta == 5):
-        quit()
+    nomeProcurado = input("Digite o nome do Cliente que deseja procurar:\n")
+    print("Procurando", nomeProcurado)
+    usuario.enviaMensagem(conexao, nomeProcurado)
+    clienteProcurado = usuario.recebeMensagem(conexao)
+    if (clienteProcurado != []):
+      print(
+          f"Cliente encontrado! \n Nome: {clienteProcurado.nome}\n IP: {clienteProcurado.ip}\n"
+      )
+    else:
+      print("Cliente com esse nome não encontrado! Tente novamente\n")
+  elif (resposta == 3):
+    pass
+  elif (resposta == 4):
+    usuario.enviaMensagem(conexao, "desligar")
+    conexao.close()
+    quit()
