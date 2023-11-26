8a35f11e8d911d5d4607f7dd0007884fe7105985
MarinaPiragibe <marinapiragibe@id.uff.br>
2023-11-26 10:50:58 -0300

    implementa cronometro

--- teste.py
+++ teste.py
@@ -1,31 +1,23 @@
 import time
+from tqdm import tqdm
 
 def cronometro():
     input("Pressione Enter para iniciar o cronômetro.")
     inicio = time.time()
 
     try:
-        while True:
+        for _ in tqdm(iterable=range(int(1e10)), unit="seg",  bar_format=" "):
             tempo_passado = time.time() - inicio
             horas, minutos = divmod(tempo_passado, 3600)
             minutos, segundos = divmod(minutos, 60)
             horas = int(horas)
             minutos = int(minutos)
             segundos = int(segundos)
-            
-            # Limpa a tela do console para atualizar o tempo
-            print("\033c", end="")
 
-            # Formata e exibe o tempo decorrido
-            print("Chamada em andamento... ")
-            print(f"{horas:02d}:{minutos:02d}:{segundos:02d}", end="", flush=True)
-
-            print("\n Ctrl+C para encerrar a chamada")
-
+            print(f"\rChamada em andamento... {horas:02d}:{minutos:02d}:{segundos:02d}", end="")
             time.sleep(1)
     except KeyboardInterrupt:
-        print("Cronômetro interrompido.")
-
+        print("\nCronômetro interrompido.")
 
 if __name__ == "__main__":
     cronometro()
